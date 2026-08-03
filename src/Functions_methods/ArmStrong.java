48f1d8f403d224728cb83c4bfe2a282f2855abef
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-03 14:59:21 +0530

    added prime numbers and armstrong numbers

---
+++ src/Functions_methods/ArmStrong.java
@@ -0,0 +1,28 @@
+package Functions_methods;
+
+import java.util.Scanner;
+
+public class ArmStrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        boolean ans = isArmstrong(n);
+        System.out.println(ans);
+
+    }
+
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n= n/10;
+            sum = sum + rem * rem * rem;
+        }
+        return sum == original;
+
+
+    }
+}
+
