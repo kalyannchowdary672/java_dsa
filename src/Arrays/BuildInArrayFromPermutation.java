c060f756ebbb193726fdb2f32f920f82bffbd23a
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-15 14:52:02 +0530

    leetcode built in array from permutaion

---
+++ src/Arrays/BuildInArrayFromPermutation.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class BuildInArrayFromPermutation {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int[] ans = new int[nums.length];
+            for(int i = 0 ;i < nums.length; i++){
+                ans[i] = nums[nums[i]];
+
+            }
+            return ans;
+
+        }
+
+    }
+
+
+
+
+
+
+}
