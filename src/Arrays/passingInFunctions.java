ec1fee76b75a20c103d20bf132eb355e310b30f4
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-21 16:00:25 +0530

    completed no of identical problems in leetcode 1512

--- src/Arrays/passingInFunctions.java
+++ src/Arrays/passingInFunctions.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class passingInFunctions {
     public static void main(String[] args) {
         int[] num = {2 , 8 ,9, 0};
-        System.out.println(Arrays.toString(num));
+        System.out.print(Arrays.toString(num));
         Change(num);
-        System.out.println(Arrays.toString(num));
+        System.out.print(Arrays.toString(num));
 
     }
     static void Change(int[] arr) {
