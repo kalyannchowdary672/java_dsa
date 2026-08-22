16a6785358358501a5ea6b66d18227666dfe5e77
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-22 16:27:57 +0530

    leetcode 1389 create a target array of given order is completed

---
+++ src/LeetCode/Arrays/Arrays/CreateTargetArrayInGivenOrder.java
@@ -0,0 +1,19 @@
+package LeetCode.Arrays.Arrays;
+
+import java.util.ArrayList;
+
+public class CreateTargetArrayInGivenOrder {
+    class Solution {
+        public int[] createTargetArray(int[] nums, int[] index) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < nums.length ; i++){
+                list.add(index[i] , nums[i]);
+            }
+            int[] target = new int[nums.length];
+            for(int j = 0 ; j < nums.length ; j++){
+                target[j] = list.get(j);
+            }
+            return target;
+        }
+    }
+}
