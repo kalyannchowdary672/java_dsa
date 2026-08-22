5419c67f77ee4f26d73f803024ba35cc6624994f
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-23 00:19:23 +0530

    leetcode peak of mountain problem completed

---
+++ src/BinarySearch/LeetcodePeakMountain.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class LeetcodePeakMountain {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int start = 0 ;
+            int end = arr.length -1;
+            while(start < end){
+                int mid = start + (end -start)/2;
+                if(arr[mid] > arr[mid+1]){
+                    // this is the dec part of array
+                    //look at left side of the array
+                    end= mid;
+                }else{
+                    // this is the ascending part of the array
+                    // look at right side of an array
+                    start = mid + 1;
+
+                }
+            }
+            return start;
+
+        }
+
+    }
+}
