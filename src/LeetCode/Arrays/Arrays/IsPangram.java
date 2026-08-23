14da15f227c84b3ea6224c75cdfa6be4b1f6cf28
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-08-23 21:57:06 +0530

    leetcode 1832 is pangram is solved

---
+++ src/LeetCode/Arrays/Arrays/IsPangram.java
@@ -0,0 +1,16 @@
+package LeetCode.Arrays.Arrays;
+
+public class IsPangram {
+    class Solution {
+        public boolean checkIfPangram(String sentence){
+            for(char ch = 'a' ; ch <= 'z' ; ch++){
+                if(sentence.indexOf(ch) == -1){
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+    }
+}
