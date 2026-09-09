6f70481ab7d3e15e6f5b894748702c43db2e8596
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-09-09 23:54:24 +0530

    insertion sort concept and example

---
+++ src/SortingAlgorithms/InsertionSortExm.java
@@ -0,0 +1,26 @@
+package SortingAlgorithms;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static java.util.Collections.swap;
+
+public class InsertionSortExm {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 5};
+        insertion(arr);
+        System.out.print(Arrays.toString(arr));
+
+    }
+    static void insertion(int[] arr){
+        for(int i = 0 ; i < arr.length -1 ; i++ ){
+            for (int j = i + 1; j > 0 ; j--){
+                if(arr[j] < arr[j-1]){
+                    swap(arr, j , j-1);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
