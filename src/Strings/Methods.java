b394335fb8b3c0fcf11d68a8c577cc1a0cb3e8d1
kalyannchowdary672 <kalyanchowdary672@gmail.com>
2026-09-18 01:42:52 +0530

    cpmpleted string topics and learned how strings work

---
+++ src/Strings/Methods.java
@@ -0,0 +1,11 @@
+package Strings;
+import java.util.Arrays;
+
+public class Methods {
+    static void main(String[] args) {
+        String name = "kalyan chowdary";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name);
+        System.out.println(name.indexOf('a'));
+    }
+}
---
+++ src/Strings/PrintStream.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class PrintStream {
+    public static void main(String[] args) {
+        float a = 123.456F;
+        System.out.printf("number at %.2f",a);
+        // place holder in java in strings
+        System.out.printf("hello my name %s and i am very %s","kalyan","cool");
+//        There are many format specifiers we can use. Here are some common ones:
+//
+//                %c - Character
+//                %d - Decimal number (base 10)
+//                %e - Exponential floating-point number
+//                %f - Floating-point number
+//                %i - Integer (base 10)
+//                %o - Octal number (base 8)
+//                %s - String
+//                %u - Unsigned decimal (integer) number
+//                %x - Hexadecimal number (base 16)
+//                %t - Date/time
+//                %n - Newline
+    }
+}
